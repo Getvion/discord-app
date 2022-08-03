4599de61a37ae5e8627bc038ea1996461cc61ac2
Oleg Somov <gog0125@mail.ru>
2022-08-03 23:59:46 +0300

    feat: add registration on backend side with jwt token

---
+++ discord-backend/controllers/auth/postLogin.js
@@ -0,0 +1,31 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const postLogin = async (req, res) => {
+  try {
+    const { mail, password } = req.body;
+
+    const user = await User.findOne({ mail: mail.toLowerCase() });
+
+    if (user && bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ userId: user._id, mail }, process.env.TOKEN_KEY, {
+        expiresIn: '24h'
+      });
+
+      return res.status(200).json({
+        userDetails: {
+          mail: user.mail,
+          token: token,
+          username: user.username
+        }
+      });
+    }
+
+    return res.status(400).send('invalid credentials, please try again');
+  } catch (error) {
+    return res.status(500).send('something went wrong, please try again');
+  }
+};
+
+module.exports = postLogin;
---
+++ discord-backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// register the routes
+app.use('/api/auth', authRoutes);
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is started on ${PORT}`);
+    });
+  })
+  .catch((err) => console.log('db connection failed, server not started', err));
