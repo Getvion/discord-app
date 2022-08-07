2cac7414a18a5b058533a3fd8a4f4409c00a963f
Oleg Somov <gog0125@mail.ru>
2022-08-07 21:40:46 +0300

    feat: add AuthBox, CustomButton, InputLabel, Redirect components

---
+++ discord-frontend/src/shared/components/index.js
@@ -0,0 +1,4 @@
+export { AuthBox } from './AuthBox';
+export { InputWithLabel } from './InputWithLabel';
+export { CustomButton } from './CustomButton';
+export { RedirectInfo } from './RedirectInfo';
