9756fe7580496729cce003ff7bd3c7fac6e4b975
Oleg Somov <gog0125@mail.ru>
2022-08-07 21:39:33 +0300

    feat: add routes for login, registration and dashboard

---
+++ discord-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { Login, Register, Dashboard } from './pages';
+
+const App = () => (
+  <div className='app'>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='*' element={<Navigate to='/dashboard' />} />
+    </Routes>
+  </div>
+);
+
+export default App;
